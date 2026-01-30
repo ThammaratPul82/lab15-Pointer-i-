4232eb31d49696fefcacfd5a895e72e7debe2903
Thammarat Pulthawonsin <tmrptws@MacBook-Air-khxng-Thammarat.local>
2026-01-30 16:00:04 +0700

    completed lab15_4

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,17 @@ int main(){
 }
 
 //Do not modify source code above this line
+void shuffle(int &a,int &b,int &c,int &d){
+	int z[]={a,b,c,d};
+	for (int i = 0; i < 4; i++) {
+        int random_index = rand() % 4; // สุ่มเลข 0-3
+        swap(z[i],z[random_index]);
+    }
+
+    // 3. เอาค่าที่สลับเสร็จแล้ว ยัดกลับคืนตัวแปรทั้ง 4
+    a = z[0];
+    b = z[1];
+    c = z[2];
+    d = z[3];
+	
+}
